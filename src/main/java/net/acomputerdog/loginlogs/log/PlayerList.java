c4135c40a8ca0db1f6810d2f233b1ad24a894308
chris <acomputerdog@gmail.com>
2019-02-09 11:38:57 -0500

    Convert to maven project

---
+++ src/main/java/net/acomputerdog/loginlogs/log/PlayerList.java
@@ -0,0 +1,124 @@
+package net.acomputerdog.loginlogs.log;
+
+import net.acomputerdog.loginlogs.main.LLPlayer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class PlayerList {
+    public static final int MAX_LOGINS = 5;
+
+    private final List<LLPlayer> playerList = new ArrayList<>();
+    private final Map<String, LLPlayer> uuidMap = new HashMap<>();
+    private final Map<String, LLPlayer> nameMap = new HashMap<>();
+    private final List<LLPlayer> loginList = new ArrayList<>(MAX_LOGINS + 1);
+    private final List<LLPlayer> logoutList = new ArrayList<>(MAX_LOGINS + 1);
+    private final Logger logger;
+
+    public PlayerList(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void load(BufferedReader in) {
+        in.lines().forEach(line -> {
+            String[] parts = line.split(":");
+            if (parts.length == 4 || parts.length == 5) {
+                try {
+                    String uuid = parts[0];
+                    String name = parts[1];
+                    long lastLogin = Long.parseLong(parts[2]);
+                    long lastLogout = Long.parseLong(parts[3]);
+                    long firstLogin;
+                    if (parts.length == 5) {
+                        firstLogin = Long.parseLong(parts[4]);
+                    } else {
+                        firstLogin = lastLogin;
+                    }
+                    LLPlayer player = new LLPlayer(uuid, name, lastLogin, lastLogout, firstLogin);
+                    registerPlayer(player);
+                } catch (NumberFormatException e) {
+                    logger.warning("Malformed line: \"" + line + "\"");
+                }
+            } else {
+                logger.warning("Malformed line: \"" + line + "\"");
+            }
+        });
+        logger.info("Loaded " + uuidMap.size() + " player records."); //can't write a counter variable from a lambada...
+    }
+
+    public void save(Writer out) {
+        try {
+            for (int index = 0; index < playerList.size(); index++) {
+                if (index > 0) {
+                    out.write("\n");
+                }
+                LLPlayer player = playerList.get(index);
+                out.write(player.toString());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Exception saving player logs!", e);
+        }
+    }
+
+    public void registerPlayer(LLPlayer player) {
+        uuidMap.put(player.getUuid(), player);
+        nameMap.put(player.getName(), player);
+        playerList.add(player);
+    }
+
+    public LLPlayer getByUUID(String uuid) {
+        return uuidMap.get(uuid);
+    }
+
+    public LLPlayer getByName(String name) {
+        return nameMap.get(name);
+    }
+
+    public LLPlayer getOrCreate(String uuid, String name) {
+        LLPlayer player = getByUUID(uuid);
+        if (player == null) {
+            player = new LLPlayer(uuid, name);
+            registerPlayer(player);
+        }
+        return player;
+    }
+
+    public LLPlayer getOrCreate(String uuid) {
+        return getOrCreate(uuid, uuid);
+    }
+
+    public LLPlayer find(String id) {
+        LLPlayer player = getByUUID(id);
+        if (player == null) {
+            player = getByName(id);
+        }
+        return player;
+    }
+
+    public List<LLPlayer> getRecentLogins() {
+        return Collections.unmodifiableList(loginList);
+    }
+
+    public List<LLPlayer> getRecentLogouts() {
+        return Collections.unmodifiableList(logoutList);
+    }
+
+    public void updateRecentLogins(LLPlayer player) {
+        loginList.remove(player);
+        loginList.add(0, player);
+        while (loginList.size() > MAX_LOGINS) {
+            loginList.remove(loginList.size() - 1);
+        }
+    }
+
+    public void updateRecentLogouts(LLPlayer player) {
+        logoutList.remove(player);
+        logoutList.add(0, player);
+        while (logoutList.size() > MAX_LOGINS) {
+            logoutList.remove(logoutList.size() - 1);
+        }
+    }
+}
---
+++ src/main/java/net/acomputerdog/loginlogs/main/LLPlayer.java
@@ -0,0 +1,96 @@
+package net.acomputerdog.loginlogs.main;
+
+public class LLPlayer implements Comparable<LLPlayer> {
+    public static final long NEVER = -1L;
+
+    private final String uuid;
+    private String name;
+
+    private long lastLogin;
+    private long firstLogin;
+    private long lastLogout;
+
+    public LLPlayer(String uuid, String name, long lastLogin, long lastLogout, long firstLogin) {
+        this.uuid = uuid;
+        this.name = name;
+        this.lastLogin = lastLogin;
+        this.firstLogin = firstLogin;
+        this.lastLogout = lastLogout;
+    }
+
+    public LLPlayer(String uuid, String name) {
+        this(uuid, name, NEVER, NEVER, NEVER);
+    }
+
+    public LLPlayer(String uuid) {
+        this(uuid, uuid);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(long loginTime) {
+        this.lastLogin = loginTime;
+    }
+
+    public long getFirstLogin() {
+        return firstLogin;
+    }
+
+    public void setFirstLogin(long firstLogin) {
+        this.firstLogin = firstLogin;
+    }
+
+    public long getLastLogout() {
+        return lastLogout;
+    }
+
+    public void setLastLogout(long lastLogout) {
+        this.lastLogout = lastLogout;
+    }
+
+    public String getCombinedName() {
+        return name + " (" + uuid + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LLPlayer)) return false;
+
+        LLPlayer llPlayer = (LLPlayer) o;
+
+        return uuid.equals(llPlayer.uuid) && name.equals(llPlayer.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uuid.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return uuid + ":" + name + ":" + lastLogin + ":" + lastLogout + ":" + firstLogin;
+    }
+
+    @Override
+    public int compareTo(LLPlayer o) {
+        return (int) (o.lastLogin - lastLogin);
+    }
+}
